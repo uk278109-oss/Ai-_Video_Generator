e1620b6575de2da18edc5eb46202e971c60a26bd
Nasir Dev <uk278109@gmail.com>
2026-09-26 09:30:58 +0500

    Delete android directory

--- android/gradlew.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set DIRNAME=%~dp0
-if "%JAVA_HOME%"=="" (set JAVA=java) else (set JAVA=%JAVA_HOME%\bin\java.exe)
-"%JAVA%" -classpath "%DIRNAME%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
